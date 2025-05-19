b72158612c85fd1351db66ea70dc10989f88299e
NTnik <147279010+Kolyan1038@users.noreply.github.com>
2025-05-19 19:44:23 +0300

    edit

--- src/shell.c
+++ src/shell.c
@@ -1,15 +1,3 @@
-#include "../include/fs_init.h"
-// gcc shell.c -o FS `pkg-config fuse --cflags --libs`
-
-//MAIN FS
-// ./shell -f /home/alexander/mnt
-// fusermount -u /home/alexander/mnt
-
-//SDCARD
-// sudo mount /dev/mmcblk0p1 /home/alexander/sdCard/
-// sudo ./shell -f -o allow_other /home/alexander/sdCard/mnt/
-// sudo fusermount -u /home/alexander/sdCard/mnt/
-
 #include <stdio.h>
 #include "../include/fs_init.h"
 
